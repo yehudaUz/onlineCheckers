54b9bbae49cb74a8c339e5aea65cb8e559049237
mudale222 <yuziel@walla.co.il>
2020-03-05 20:26:03 +0200

    minor style and pm2 testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'API',
-    script: 'env-cmd -f ./config/dev.env ./game/app.js',
+    script: './game/app.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
@@ -10,6 +10,9 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
+      SENDGRID_API_KEY: "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",
+      MONGODB_URL: "mongodb + srv://mudale:dualcore@cluster0-lgfig.mongodb.net/user-database",
+      JWT_SECRET: "kjh23!@$2#lkasDASvBdd",
       NODE_ENV: 'development'
     },
     env_production: {
@@ -17,14 +20,14 @@ module.exports = {
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
