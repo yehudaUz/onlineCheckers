afbfe48bfb6d4f4905fece7fbb070c0863a6b052
mudale222 <yuziel@walla.co.il>
2020-03-05 20:16:13 +0200

    minor style and pm2 testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: './game/app.js',
+    script: 'app.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
